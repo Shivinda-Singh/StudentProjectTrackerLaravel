bbfe0f2fc5f8c62a39eedac3efead16965abb214
Tsai Chang <tsai.chang94@gmail.com>
2017-04-18 17:24:10 -0400

    fix the col size

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -13,9 +13,8 @@
        <br>
 <p class=" text-info">May 05,2014,03:00 pm </p>
       </div>-->
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-   
-   
+        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xs-offset-0 col-sm-offset-0 col-md-offset-2 col-lg-offset-2 toppad" >
+
           <div class="panel panel-info">
             <div class="panel-heading">
               <h2>{{$project->name}}</h2>
