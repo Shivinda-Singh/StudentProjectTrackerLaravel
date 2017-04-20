d148643df9089b9d3238f6296a6e2544c938f477
haifeng <haifengmei@hotmail.com>
2017-04-20 14:05:24 -0400

    added sql

--- studentProjectTracker.sql
+++ studentProjectTracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2017 at 03:37 AM
+-- Generation Time: Apr 20, 2017 at 08:04 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -19,6 +19,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `studentprojecttracker`
 --
+CREATE DATABASE IF NOT EXISTS `studentprojecttracker` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `studentprojecttracker`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(2, 'Admin Guy', 'datguy@admin.com', '$2y$10$kkwsWIUexQLjoHRGwnE5F.HKPSyTOtWQv6nlBn1t/2I9CtOOLMmVK', NULL, '2017-04-19 23:05:37', '2017-04-19 23:05:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -36,9 +76,14 @@ CREATE TABLE `migrations` (
 -- Dumping data for table `migrations`
 --
 
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES(19, '2014_10_12_000000_create_users_table', 1);
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES(20, '2014_10_12_100000_create_password_resets_table', 1);
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES(21, '2017_04_16_213117_create_projects_table', 1);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(51, '2014_10_12_000000_create_users_table', 1),
+(52, '2014_10_12_100000_create_password_resets_table', 1),
+(53, '2017_04_16_213117_create_projects_table', 1),
+(54, '2017_04_18_005824_create_comments_table', 1),
+(55, '2017_04_19_023819_create_tags_table', 2),
+(56, '2017_04_19_023819_create_students_table', 3),
+(57, '2017_04_19_164840_create_admins_table', 4);
 
 -- --------------------------------------------------------
 
@@ -52,6 +97,13 @@ CREATE TABLE `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('datguy@admin.com', '$2y$10$2jMeMWybIKyEQ0vd81/rjeHRmVXFRdrhm8FEXgHQWM2ryEACB/Voq', '2017-04-20 01:02:00');
+
 -- --------------------------------------------------------
 
 --
@@ -59,60 +111,100 @@ CREATE TABLE `password_resets` (
 --
 
 CREATE TABLE `projects` (
-  `pid` int(10) UNSIGNED NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `course_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `year` int(11) NOT NULL,
-  `github` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `completed` tinyint(1) NOT NULL DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `collaborators` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `course_code` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year_completed` int(11) NOT NULL,
+  `approved` tinyint(1) NOT NULL DEFAULT '0',
+  `rejection_reasons` longtext COLLATE utf8mb4_unicode_ci,
+  `pending` tinyint(1) NOT NULL DEFAULT '1',
+  `github` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`pid`, `title`, `description`, `course_code`, `year`, `github`, `completed`, `created_at`, `updated_at`) VALUES(1, 'Go-Ceries', 'Online grocery shopping app', 'INFO3490', 3, 'https://go-ceries.firebaseapp.com', 0, '2017-04-16 23:00:49', '2017-04-16 23:00:49');
-INSERT INTO `projects` (`pid`, `title`, `description`, `course_code`, `year`, `github`, `completed`, `created_at`, `updated_at`) VALUES(2, 'Sports Junkies', 'Sports news app', 'INFO3490', 3, 'sportsjunkies.com', 0, '2017-04-16 23:00:49', '2017-04-16 23:00:49');
-INSERT INTO `projects` (`pid`, `title`, `description`, `course_code`, `year`, `github`, `completed`, `created_at`, `updated_at`) VALUES(3, 'Junk Trade', 'Item trading app', 'INFO3490', 3, 'junktrades.com', 1, '2017-04-16 23:01:03', '2017-04-16 23:01:03');
+INSERT INTO `projects` (`id`, `user_id`, `name`, `description`, `collaborators`, `course_code`, `year_completed`, `approved`, `rejection_reasons`, `pending`, `github`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Go-ceries', 'a online grocery app', 'TSM', 'INFO3490', 3454, 1, NULL, 0, 'go-ceries.firebaseapp.com', '2017-04-19 00:19:55', '2017-04-19 00:19:55'),
+(2, 2, 'Junk Trades', 'an item trading app', 'Traders', 'INFO3490', 6456, 1, NULL, 0, 'junktrades.com', '2017-05-19 00:34:47', '2017-04-20 10:19:14'),
+(3, 1, 'Sports Junkies', 'a sports news app', 'Junkies', 'INFO3490', 4656, 0, NULL, 1, 'sportsjunkies.com', '2016-05-19 00:35:02', '2017-04-19 00:35:02'),
+(4, 1, 'Student Project Tracker', 'a app for tracking student projects', 'peeeps', 'INFO 3490', 2017, 0, NULL, 1, 'studentprojecttracker.com', '2017-04-19 03:28:41', '2017-04-19 03:28:41');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Table structure for table `project_tag`
 --
 
-CREATE TABLE `students` (
-  `sid` int(10) UNSIGNED NOT NULL,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `degree` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `year` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `project_tag` (
+  `project_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
 
 --
--- Dumping data for table `students`
+-- Dumping data for table `project_tag`
 --
 
-INSERT INTO `students` (`sid`, `name`, `degree`, `year`, `created_at`, `updated_at`) VALUES(814000650, 'Haifeng Mei', 'BSc Information Technology', 2014, '2017-03-31 01:09:09', NULL);
-INSERT INTO `students` (`sid`, `name`, `degree`, `year`, `created_at`, `updated_at`) VALUES(814002678, 'Aaron Sookoo', 'BSc Information Technology', 2014, '2017-03-31 01:09:10', NULL);
-INSERT INTO `students` (`sid`, `name`, `degree`, `year`, `created_at`, `updated_at`) VALUES(814002710, 'Nickolas Barran', 'BSc Information Technology', 2014, '2017-04-17 21:51:38', '2017-04-17 21:51:38');
-INSERT INTO `students` (`sid`, `name`, `degree`, `year`, `created_at`, `updated_at`) VALUES(814003006, 'Shivinda Singh', 'BSc Information Technology', 2014, '2017-03-31 01:09:10', NULL);
-INSERT INTO `students` (`sid`, `name`, `degree`, `year`, `created_at`, `updated_at`) VALUES(814004302, 'Tsai-Ann Chang', 'BSc Information Technology', 2014, '2017-03-31 01:09:10', NULL);
+INSERT INTO `project_tag` (`project_id`, `tag_id`) VALUES
+(1, 2),
+(1, 3),
+(2, 4),
+(4, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `team`
+-- Table structure for table `project_user`
 --
 
-CREATE TABLE `team` (
-  `sid` int(11) NOT NULL,
-  `pid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+CREATE TABLE `project_user` (
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project_user`
+--
+
+INSERT INTO `project_user` (`user_id`, `project_id`) VALUES
+(1, 1),
+(1, 4),
+(1, 5),
+(1, 13),
+(1, 14),
+(2, 1),
+(2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'laravel', '2017-04-19 21:33:37', '2017-04-19 21:33:37'),
+(2, 'vue', '2017-04-19 21:33:37', '2017-04-19 21:33:37'),
+(3, 'javascript', '2017-04-19 21:33:48', '2017-04-19 21:33:48'),
+(4, 'php', '2017-04-19 21:33:48', '2017-04-19 21:33:48'),
+(5, 'angular', '2017-04-19 21:47:02', '2017-04-19 21:47:02'),
+(6, 'slim', '2017-04-19 21:47:02', '2017-04-19 21:47:02');
 
 -- --------------------------------------------------------
 
@@ -131,8 +223,29 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Feng', 'Feng@lol.com', '$2y$10$Y9u/EnHjwAWeJDcJ8teh8.AQ3DU5xBqeBnneHP25vO8ZFW5XbMPpG', 'POoOX5wjZgnesYHuyQ56ebpSwBSCqhjoaevK4lf2RZhud9BJrrVGlC7S7Xgi', '2017-04-19 00:17:14', '2017-04-19 00:17:14'),
+(2, 'john cena', 'john@cena.com', '$2y$10$YvVXEvwn0pGzjbkhFc/wMuFQPkBmvDBhDNtt1qT5OCt3Tt9F8Zw9K', '3LKpJVLwoqt2al9FS1C1eb9UlIsOjCpKgg4i9d0ZSrq7LXgpe8kBjEqSQ9yi', '2017-04-19 00:41:47', '2017-04-19 00:41:47');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admins_email_unique` (`email`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -150,20 +263,26 @@ ALTER TABLE `password_resets`
 -- Indexes for table `projects`
 --
 ALTER TABLE `projects`
-  ADD PRIMARY KEY (`pid`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `students`
+-- Indexes for table `project_tag`
 --
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`sid`),
-  ADD UNIQUE KEY `id` (`sid`);
+ALTER TABLE `project_tag`
+  ADD PRIMARY KEY (`project_id`,`tag_id`);
 
 --
--- Indexes for table `team`
+-- Indexes for table `project_user`
 --
-ALTER TABLE `team`
-  ADD PRIMARY KEY (`sid`,`pid`);
+ALTER TABLE `project_user`
+  ADD PRIMARY KEY (`user_id`,`project_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `users`
@@ -177,25 +296,35 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `pid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT for table `tags`
 --
-ALTER TABLE `students`
-  MODIFY `sid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=814004303;
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
